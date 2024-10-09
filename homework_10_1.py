a3ab37dc60e40ab72d6a49d8f59437125f9e4580
AlfaCentawra-coder <vovaluzin55@gmail.com>
2024-10-09 16:35:56 +0500

    Add files via upload

---
+++ homework_10_1.py
@@ -0,0 +1,39 @@
+from threading import Thread
+from time import sleep, time
+
+def write_words(word_count, file_name):
+    with open(file_name, 'w') as file:
+        for i in range(1, word_count + 1):
+            file.write(f'Какое-то слово № {i}\n')
+            sleep(0.1)
+    print(f"Завершилась запись в файл {file_name}")
+
+start_time = time()
+write_words(10, 'example1.txt')
+write_words(30, 'example2.txt')
+write_words(200, 'example3.txt')
+write_words(100, 'example4.txt')
+end_time = time()
+difference_time = end_time - start_time
+print(f"Работа потоков {difference_time} секунд")
+
+thread1 = Thread(target=write_words, args=(10, 'example5.txt'))
+thread2 = Thread(target=write_words, args=(30, 'example6.txt'))
+thread3 = Thread(target=write_words, args=(200, 'example7.txt'))
+thread4 = Thread(target=write_words, args=(100, 'example8.txt'))
+
+start_time = time()
+thread1.start()
+thread2.start()
+thread3.start()
+thread4.start()
+
+thread1.join()
+thread2.join()
+thread3.join()
+thread4.join()
+end_time = time()
+difference_time = end_time - start_time
+print(f"Работа потоков {difference_time} секунд")
+
+
